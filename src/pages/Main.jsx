ae75841e430c00fe0186783b4da03da61f2f0667
kjh27ss <kjh27ss@naver.com>
2023-12-13 19:44:53 +0900

    choi : 충돌 부분 수정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import { useAuthValue } from '../context/AuthProvider'
-
 import TopNav from '../layout/TopNav'
 import LoggedTop from '../layout/LoggedTop'
 import Search from '../layout/Search'
 import Footer from '../layout/Footer'
-import ApiList from '../components/ApiList'
 import Info from '../components/Info'
-import Cservice from '../layout/Cservice'
 
 
 const Main = () => {
@@ -19,18 +16,14 @@ const Main = () => {
         <>
           <LoggedTop/>
           <Search/>
-          <ApiList/>
           <Info />
-          <Cservice/>
           <Footer/>
         </>
         ):(
           <>
             <TopNav/>
             <Search/>
-            <ApiList/>
             <Info />
-            <Cservice/>
             <Footer/>
           </>
         )

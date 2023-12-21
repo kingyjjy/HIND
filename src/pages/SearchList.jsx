c8587518c5f61a3dcd0bd78256da3277cfaf7035
kjh27ss <kjh27ss@naver.com>
2023-12-21 17:27:40 +0900

    choi:검색 페이지 구현

--- src/pages/SearchList.jsx
+++ src/pages/SearchList.jsx
@@ -6,70 +6,58 @@ import Footer from '../layout/Footer'
 import {BsSearch} from 'react-icons/bs'
 import '../assets/css/searchlist.css';
 
-// const user = [
-//   { id:1 , name: '이나영'},
-//   { id:2 , name: '김태희'},
-//   { id:3 , name: '다음'},
-//   { id:4 , name: '네이버'},
-//   { id:5 , name: 'daum'},
-//   { id:6 , name: 'Naver'},
-//   { id:7 , name: 'GOOGLE'},
-//   { id:8 , name: '김다미'},
-//   { id:9 , name: '다람쥐'},
-//   { id:10 , name: 'TIGER'},
-//   { id:11 , name: '홍대'},
-//   { id:12 , name: '강남'},
-//   { id:13 , name: '영등포'},
-// ]
-
 const SearchList = ({burl}) => {
   const [data,setData] = useState([]); // api data 담기
   const [text, setText] = useState(''); // input 입력되는 글자
-  const [filters, setFilters] = useState(data); // 필터링 글자
+  const [filters, setFilters] = useState(data); // 필터된 data
 
-  console.log(data);
-
-  useEffect(()=> {
     const getSearch = async() => {
       try{
           const res = await axios.get(burl);
           const test = res.data.SynthesizeHospital[1].row;
+
+          const result = test.filter(elem => elem.TREAT_SBJECT_CONT !== null);
   
-          setData(test);
-          console.log(data);
+          setData(result);
       }catch(e){
           console.log(e);
       }
-    }
-  })
+  }
+  useEffect(() => {
+    getSearch();
+  }, [])
   
+  const handleChange = (e) => {
+    setText(e.target.value);
+  }
 
-  // useEffect(() => {
-  //   setFilters(data.filter(item => item.TREAT_SBJECT_CONT.includes(text.toLowerCase())))
-  // }, [text])
+  const handleClick = () => {
+    setFilters(data.filter(elem => elem.BIZPLC_NM.includes(text.toLowerCase())))
+  }
+
+  const onSubmitSearch = (e) => {
+    if(e.key === 'Enter'){
+      setFilters(data.filter(elem => elem.BIZPLC_NM.includes(text.toLowerCase())))
+    }
+  }
 
 
   return (
     <>
         <TopNav/>
-        <div className='container'>
+        <div className='container d-flex flex-column min-vh-100'>
             <div className="search-world position-relative mt-5 d-flex">
-              <input type="text" value={text} placeholder='검색어를 입력하세요.' className='col-8 searchbox form-control' onChange={e => setText(e.target.value)}/>
-              <button type="submit" className=" btn col-2 m-1" >
+              <input type="text" value={text} placeholder='검색어를 입력하세요.' className='col-8 searchbox form-control' onChange={handleChange} onKeyPress={onSubmitSearch}/>
+              <button type="submit" className=" btn col-2 m-1" onClick={handleClick} >
                 <BsSearch size={23} className='sicon position-absolute'/>
               </button>
             </div>
             <div className="obj-list mt-5 mb-5">
             <div className='list-header'><a href="#">병원명</a><span>주소</span></div>
-            {/* {data.map((item, idx)=>{
-                            return <div className='lists' key={idx}>
-                              <Link to="/detail" state={{ title : blogData.BIZPLC_NM, address : blogData.REFINE_LOTNO_ADDR, lat : blogData.REFINE_WGS84_LAT, logt : blogData.REFINE_WGS84_LOGT}}>{blogData.BIZPLC_NM}</Link>
-                                  <span>{blogData.REFINE_LOTNO_ADDR}</span>
-                              </div>;
-                        })}  */}
             {
-              filters.map(item => <div className='lists' key={item.TREAT_SBJECT_CONT}>
-                {item.TREAT_SBJECT_CONT }
+              filters.map(elem => <div className='lists' key={elem.BIZPLC_NM}>
+                <Link to="/detail" state={{ title : elem.BIZPLC_NM, address : elem.REFINE_LOTNO_ADDR, lat : elem.REFINE_WGS84_LAT, logt : elem.REFINE_WGS84_LOGT}}>{elem.BIZPLC_NM}</Link>
+                                  <span>{elem.REFINE_LOTNO_ADDR}</span>
               </div>)
             }
           </div>

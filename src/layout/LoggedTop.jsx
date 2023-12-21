d6b7aad656087544349d9d103499d24bd1652279
yjjy <cpflehwjs88@naver.com>
2023-12-21 17:26:03 +0900

    kim: navbar디자인 수정, 폰트수정'

--- src/layout/LoggedTop.jsx
+++ src/layout/LoggedTop.jsx
@@ -47,92 +47,93 @@ const LoggedTop = () => {
     
   return (
     <>  
-        <Nav className="navbar navbar-expand-lg bg-white">
+        <Navbar expand="lg" className="navbar navbar-expand-lg bg-white">
             <Container className='d-flex justify-content-between'>
-                <Link to='/' className=' link'><img src="images/logo.png" alt="" /></Link>
-                <Navbar.Toggle aria-controls='basic-navbar-nav'/>
-                {/* <Navbar.Collapse id='basic-navbar-nav'/> */}
-                <Nav className='my-3' style={{flexGrow:'0'}}>
-                    <NavDropdown  title='지역찾기' id='basci-nav-dropdown' >
-                        <div style={{width:'400px'}}>
-                            <Row className='m-3'>
-                                <Col lg='4'>
-                                    <NavDropdown.Item><Link to="/main" state={{name:data[11]}}>감정동</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/main" state={{name:data[9]}}>걸포동</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/main" state={{name:data[14]}}>고촌읍</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/main" state={{name:data[5]}}>구래동</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/main" state={{name:data[3]}}>대곶면</Link></NavDropdown.Item>
-                                    
-                                </Col>
-                                <Col lg='4'>
-                                    <NavDropdown.Item><Link to="/main" state={{name:data[6]}}>마산동</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/main" state={{name:data[10]}}>북변동</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/main" state={{name:data[12]}}>사우동</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/main" state={{name:data[4]}}>양촌읍</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/main" state={{name:data[7]}}>운양동</Link></NavDropdown.Item>
-                                    
-                                </Col>
-                                <Col lg='4'>
-                                    <NavDropdown.Item><Link to="/main" state={{name:data[0]}}>월곶면</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/main" state={{name:data[8]}}>장기동</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/main" state={{name:data[2]}}>통진읍</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/main" state={{name:data[13]}}>풍무동</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/main" state={{name:data[1]}}>하성면</Link></NavDropdown.Item>
-                                </Col>
-                            </Row>
-                        </div>
-                    </NavDropdown>
-                    <NavDropdown  title='진료과' id='basci-nav-dropdown' >
-                        <div style={{width:'700px'}}>
-                            <Row className='m-3'>
-                                <Col lg='3'>
-                                    <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[0]}}>가정의학과</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[1]}}>내과</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[2]}}>비뇨기과</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[3]}}>산부인과</Link></NavDropdown.Item>
-                                </Col>
-                                <Col lg='3'>
-                                    <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[4]}}>성형외과</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[5]}}>소아청소년과</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[6]}}>신경과</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[7]}}>신경외과</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[8]}}>안과</Link></NavDropdown.Item>
-                                </Col>
-                                <Col lg='3'>
-                                    <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[9]}}>외과</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[10]}}>이비인후과</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[11]}}>재활의학과</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[12]}}>정신건강의학과</Link></NavDropdown.Item>
-                                    
-                                </Col>
-                                <Col lg='3'>
-                                    <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[13]}}>정형외과</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[14]}}>치과</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[15]}}>피부과</Link></NavDropdown.Item>
-                                    <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[16]}}>한방과</Link></NavDropdown.Item>
-                                </Col>
-                            </Row>
-                        </div>
-                    </NavDropdown>
-                    <Nav.Link href="/healthinfo">건강정보</Nav.Link>
-                    <NavDropdown  title='서비스안내' id='basci-nav-dropdown' >
-                        <div style={{width:'100px'}}>
-                            <Row className='m-3'>
-                                <Col>
-                                    <NavDropdown.Item>FAQ</NavDropdown.Item>
-                                    <NavDropdown.Item>공지사항</NavDropdown.Item>
-                                </Col>
-                            </Row>
-                        </div>
-                    </NavDropdown>
-                    
-                </Nav>
-                <div className='d-flex'>
-                    <button type='button' className='btn me-2' onClick={handelShow}><GrUser/></button>
-                    <button type="button" onClick={logout} className='btn btn-outline-primary outbox'>로그아웃</button>
-                </div>
+                <Navbar.Brand to='/' className='col-3 link'><img src="images/logo.png" alt="logo" /></Navbar.Brand>
+                <Navbar.Toggle aria-controls="navtoggle"/>
+                <Navbar.Collapse id="navtoggle">
+                    <Nav className='col-9 my-3' style={{flexGrow:'0'}}>
+                        <NavDropdown  title='지역찾기' id='basci-nav-dropdown' >
+                            <div style={{width:'400px'}}>
+                                <Row className='m-3'>
+                                    <Col lg='4'>
+                                        <NavDropdown.Item><Link to="/main" state={{name:data[11]}}>감정동</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to="/main" state={{name:data[9]}}>걸포동</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to="/main" state={{name:data[14]}}>고촌읍</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to="/main" state={{name:data[5]}}>구래동</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to="/main" state={{name:data[3]}}>대곶면</Link></NavDropdown.Item>
+                                        
+                                    </Col>
+                                    <Col lg='4'>
+                                        <NavDropdown.Item><Link to="/main" state={{name:data[6]}}>마산동</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to="/main" state={{name:data[10]}}>북변동</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to="/main" state={{name:data[12]}}>사우동</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to="/main" state={{name:data[4]}}>양촌읍</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to="/main" state={{name:data[7]}}>운양동</Link></NavDropdown.Item>
+                                        
+                                    </Col>
+                                    <Col lg='4'>
+                                        <NavDropdown.Item><Link to="/main" state={{name:data[0]}}>월곶면</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to="/main" state={{name:data[8]}}>장기동</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to="/main" state={{name:data[2]}}>통진읍</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to="/main" state={{name:data[13]}}>풍무동</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to="/main" state={{name:data[1]}}>하성면</Link></NavDropdown.Item>
+                                    </Col>
+                                </Row>
+                            </div>
+                        </NavDropdown>
+                        <NavDropdown  title='진료과' id='basci-nav-dropdown' >
+                            <div style={{width:'700px'}}>
+                                <Row className='m-3'>
+                                    <Col lg='3'>
+                                        <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[0]}}>가정의학과</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[1]}}>내과</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[2]}}>비뇨기과</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[3]}}>산부인과</Link></NavDropdown.Item>
+                                    </Col>
+                                    <Col lg='3'>
+                                        <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[4]}}>성형외과</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[5]}}>소아청소년과</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[6]}}>신경과</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[7]}}>신경외과</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[8]}}>안과</Link></NavDropdown.Item>
+                                    </Col>
+                                    <Col lg='3'>
+                                        <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[9]}}>외과</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[10]}}>이비인후과</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[11]}}>재활의학과</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[12]}}>정신건강의학과</Link></NavDropdown.Item>
+                                        
+                                    </Col>
+                                    <Col lg='3'>
+                                        <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[13]}}>정형외과</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[14]}}>치과</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[15]}}>피부과</Link></NavDropdown.Item>
+                                        <NavDropdown.Item><Link to="/objlist" state={{mobj:obj[16]}}>한방과</Link></NavDropdown.Item>
+                                    </Col>
+                                </Row>
+                            </div>
+                        </NavDropdown>
+                        <Nav.Link href="/healthinfo">건강정보</Nav.Link>
+                        <NavDropdown  title='서비스안내' id='basci-nav-dropdown' >
+                            <div style={{width:'100px'}}>
+                                <Row className='m-3'>
+                                    <Col>
+                                        <NavDropdown.Item>FAQ</NavDropdown.Item>
+                                        <NavDropdown.Item>공지사항</NavDropdown.Item>
+                                    </Col>
+                                </Row>
+                            </div>
+                        </NavDropdown>
+                        
+                    </Nav>
+                    <div className='col-6 d-flex'>
+                        <button type='button' className='btn me-2' onClick={handelShow}><GrUser/></button>
+                        <button type="button" onClick={logout} className='btn btn-outline-primary outbox'>로그아웃</button>
+                    </div>
+                </Navbar.Collapse>
             </Container>
-        </Nav>
+        </Navbar>
         <Offcanvas show={show} onHide={handleClose} placement={'end'}>
             <Offcanvas.Header closeButton>
                 
